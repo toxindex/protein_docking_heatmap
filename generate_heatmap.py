ce91b1f78958dbd3783f5f87f0e21cf758f8e620
jpmikhail <johnpmikhail@gmail.com>
2025-06-12 09:35:56 -0400

    Edited generate_heatmap.py

--- generate_heatmap.py
+++ generate_heatmap.py
@@ -211,9 +211,9 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--protein_set",
-        choices = ["thyroid", "autism"],
+        choices = ["thyroid", "autism", "cancer"],
         required = True,
-        help = "Choose which set of proteins to use: 'thyroid' or 'autism'."
+        help = "Choose which set of proteins to use: 'thyroid', 'autism', or 'cancer'."
     )
     parser.add_argument(
         "--output_dir",
